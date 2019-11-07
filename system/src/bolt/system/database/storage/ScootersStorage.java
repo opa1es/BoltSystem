035870b672484485d4094f762c63b9a7260303d2
sezare <sezare@ttu.ee>
2019-11-07 16:33:35 +0200

    added requirements, ensures, invariants as comments to dao and storage classes

--- system/src/bolt/system/database/storage/ScootersStorage.java
+++ system/src/bolt/system/database/storage/ScootersStorage.java
@@ -5,6 +5,10 @@ import bolt.system.entities.scooter.Scooter;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * invariants: scooterStorage
+ *
+ */
 public class ScootersStorage {
 
 
@@ -22,24 +26,51 @@ public class ScootersStorage {
 
     }
 
-
+    /**
+     * require - scooter not null; can make connection with DB
+     *
+     * ensure - new scooter entity should be added to scooterStorage object storage;
+     * new storage.size = old storage.size + 1
+     *
+     * @param scooter - scooter to add
+     */
     public void addNewScooter(Scooter scooter){
 
     }
 
+    /**
+     * require -  in database scooterStorage exists Scooter object with id equals param id
+     * ; can make connection with DB
+     * @param id - id of scooter to select
+     * @return scooter object with id: id
+     */
     public Scooter getScoterById(long id){
 
         return null;
     }
 
+    /**
+     * require - require in database scooterStorage exists Scooter object with id equals param id
+     * ; can make connection with DB
+     * @param id - scooter id t delete
+     */
     public void deleteScooter(long id){
 
     }
 
+    /**
+     * require - can make connection with DB
+     *
+     * @return List of scooter objects
+     */
     public List<Scooter> getAllScooters(){
         return null;
     }
-
+    /**
+     * require - can make connection with DB
+     *
+     * @return List of scooter objects
+     */
     public List<Scooter> addAvailableScooters(){
 
         return null;
--- system/src/bolt/system/database/storage/UserStorage.java
+++ system/src/bolt/system/database/storage/UserStorage.java
@@ -1,14 +1,18 @@
 package bolt.system.database.storage;
 
+import bolt.system.entities.user.BankAccountData;
 import bolt.system.entities.user.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * invariants: userStorage
+ */
 public class UserStorage {
 
 
-    public List<User> usersStorage;
+    private List<User> usersStorage;
 
 
     public UserStorage(List<User> usersStorage) {
@@ -19,23 +23,52 @@ public class UserStorage {
         usersStorage = new ArrayList<>();
     }
 
-
+    /**
+     * require - user not null; can make connection with DB
+     *
+     * ensure - new user entity should be added to UserStorage object storage;
+     * new storage.size = old storage.size + 1
+     *
+     * @param user - user to add
+     */
     public void addNewUser(User user){
 
     }
-
+    /**
+     * require - require in database database exists User object with id equals param id
+     * ; can make connection with DB
+     * @param id - user id to delete
+     */
     public void deleteUser(long id){
 
     }
-
+    /**
+     * require -  in database database exists User object with id equals param id
+     * ; can make connection with DB
+     * @param id - id of user to select
+     * @return user object with id: id
+     */
     public User getUserById(long id){
 
         return null;
     }
-
+    /**
+     * require - can make connection with DB
+     *
+     * @return List of User objects
+     */
     public List<User> getAllUsers(){
 
         return null;
+    }
+
+    /**
+     *
+     * @param userId - user id in system
+     * @param bankAccountData - new bank account
+     */
+    public void changeUserBankAccount(long userId, BankAccountData bankAccountData){
+
     }
 
 
