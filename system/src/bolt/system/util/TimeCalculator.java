631b188c72dc6905b47c3770ea4cc1ef2c6bcb2d
sezare <sezare@ttu.ee>
2019-12-18 13:09:11 +0200

    v1.4.3.1 utils are broken! TODO: fix utils

--- system/src/bolt/system/util/TimeCalculator.java
+++ system/src/bolt/system/util/TimeCalculator.java
@@ -2,15 +2,19 @@ package bolt.system.util;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.text.DecimalFormat;
-import java.time.ZonedDateTime;
 import java.util.Date;
 
 public class TimeCalculator {
 
-
-    public static double getDifferenceInMinutes(Date datatime1, Date datatime2) {
-
-        return BigDecimal.valueOf((double) Math.abs(datatime1.getTime() - datatime2.getTime()) / (1000 * 60)).setScale(3, RoundingMode.HALF_UP).doubleValue();
+    //TODO: fix it. values overflow by some reason
+	//@ requires datatime1 != null;
+	//@ requires datatime2 != null;
+	//@ requires datatime1.getTime() >= 0L;
+	//@ requires datatime2.getTime() >= 0L;
+	//@ requires datatime1.getTime() <= Long.MAX_VALUE;
+	//@ requires datatime2.getTime() <= Long.MAX_VALUE;
+	//@ signals_only \nothing;
+	public /*@ code_safe_math @*/ static double getDifferenceInMinutes(Date datatime1, Date datatime2) { 
+        return BigDecimal.valueOf((double)  (datatime1.getTime() - datatime2.getTime())/ (1000 * 60)).setScale(3, RoundingMode.HALF_UP).doubleValue();
     }
 }
